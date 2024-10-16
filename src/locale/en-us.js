dfe1523233f3c44bbaf82945a7caf08a725e0daf
oren.zhang <oren.zhang@temu.com>
2024-10-16 17:55:37 +0800

    chore(project): init repo

---
+++ src/locale/en-us.js
@@ -0,0 +1,11 @@
+const mEnUS = {
+  Home: 'Home',
+  OVINCCN: 'OVINC CN',
+  PageNotExist: 'Page Not Exist',
+  loading: 'Loading...',
+  VerifyingUserInfo: 'Verifying...',
+  Logout: 'Logout',
+  Login: 'Login',
+};
+
+export default mEnUS;
---
+++ src/locale/zh-cn.js
@@ -0,0 +1,11 @@
+const mZhCN = {
+  Home: '首页',
+  OVINCCN: 'OVINC',
+  PageNotExist: '页面不存在，请检查访问链接',
+  loading: '加载中...',
+  VerifyingUserInfo: '登录中',
+  Logout: '登出',
+  Login: '登录',
+};
+
+export default mZhCN;
